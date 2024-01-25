fbc786d083aa1b21fa0dc5e328a23fb50920ea47
NCP08 <navercloudplatform08.gmail.com>
2024-01-25 15:11:50 +0900

    SingOperator.java 부호 연산자 예제

---
+++ Ch02_Variable/src/ch02/singOperator09/SingOperator.java
@@ -0,0 +1,14 @@
+package ch02.singOperator09;
+
+public class SingOperator {
+
+	public static void main(String[] args) {
+		int num = 100;
+		int num1 = -num;
+		int num2 = -num1;
+		System.out.println(num);
+		System.out.println(num1);
+		System.out.println(num2);
+	}
+
+}
