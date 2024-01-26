3af04bf3a0841813dae8ab5a452d00b81ad24475
NCP08 <navercloudplatform08.gmail.com>
2024-01-26 17:55:41 +0900

    배열 Arrays.sort 추가

---
+++ Ch05_Array/src/ch05/array08/Resolve1.java
@@ -0,0 +1,31 @@
+package ch05.array08;
+import java.util.Arrays;
+//1. 정수 배열 5개를 할당하고
+//Arrays.sort를 활용해서 최소값과 최대값을 얻으세요
+public class Resolve1 {
+
+	   	public static void main(String[] args) {
+	   		
+	   		int[] arr = {5,49,2,1,99};
+	   		
+	   		Arrays.sort(arr);
+	   		
+	   		System.out.println("Sorted arr[] :" + Arrays.toString(arr));
+	
+	   		int s = 
+	   		if(arr.length) 
+	   			
+	   		
+//	   		for(int i = arr.length -1; i<arr.length;i++) {
+//	   			System.out.println(arr[i]);
+	   		}
+	}
+}
+
+
+
+
+
+
+
+
---
+++ Ch05_Array/src/ch05/array08/Resolve2.java
@@ -0,0 +1,24 @@
+package ch05.array08;
+//2. 정수 배열 5개를 할당하고
+//Arrays.sort를 사용하지 말고 최소값과 최대값을 얻으세요	
+import java.util.Arrays;
+//2. String 배열을 10명의 이름으로 초기화하고 역순으로 출력하세요
+public class Resolve2 {
+	public static void main(String[] args) {
+		
+		String[] name = {"영정원","일정원","이정원","삼정원","오정원","육정원","칠정원","팔정원","구정원","열정원"};
+		System.out.println(Arrays.toString(name));
+		for(int i=name.length -1;i >= 0;i--) {
+			System.out.print(name[i]+", ");
+		}
+		/* System.out.println(); */
+	}
+}
+
+
+
+
+
+
+
+
