624f2c1d663eadb6321ba04ec0835a89d5deb0f5
NCP08 <navercloudplatform08.gmail.com>
2024-01-26 11:21:23 +0900

    for 반복문으로 구구단 만들기

---
+++ Ch03_Control/src/ch/continue11/ContinueFx.java
@@ -0,0 +1,19 @@
+package ch.continue11;
+
+public class ContinueFx {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=9;i++) {
+			System.out.printf("3*%d=%d\n",i,3*i);
+		}
+		System.out.println();
+		
+		for(int i =1;i<=9;i++) {
+			// i가 5보다 크면 아래를 실행하지말고 반복문으로 돌아가라
+			if(i>5)
+				continue;
+			System.out.printf("3*%d=%d\n",i,3*i);
+		}
+	}
+
+}
