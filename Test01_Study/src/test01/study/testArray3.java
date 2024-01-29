21aeffae73f7fc7ffe010ba9fd6c2e789a3dc317
NCP08 <navercloudplatform08.gmail.com>
2024-01-29 10:59:22 +0900

    Study Array문제 풀기

--- Test01_Study/src/test01/study/testArray3.java
+++ Test01_Study/src/test01/study/testArray3.java
@@ -3,24 +3,15 @@ package test01.study;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class testArray {
+public class testArray3 {
 
 	public static void main(String[] args) {
 
 		int[] arr = {1,5,9,3,7};
-		
-		Arrays.sort(arr);
-		int max = arr[0];
+
 		int min = arr[0];
-		
-		for(int i =0; i<arr.length;i++) {
-			if(arr[i]<min) {
-				min = arr[i];
-			}
-			if(arr[i]>max) {
-				max = arr[i];				
-			}
-		}
+		int max = arr[arr.length-1];
+
 		System.out.println("최소값: "+min+", 최대값: "+max);	
 
 		
