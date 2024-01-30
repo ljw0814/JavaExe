6cacc4d65e3a4a4a90aad72b8c411285db6242c6
ljw0814 <leejeongwon8145@gmail.com>
2024-01-30 10:10:49 +0900

    0129 메서드 문제 풀기

--- Test01_Study/src/ch06_Method01/Resolve5.java
+++ Test01_Study/src/ch06_Method01/Resolve5.java
@@ -10,8 +10,14 @@ import java.util.Scanner;
 	위처럼 출력되는 void star(int num) 메서드를 작성하고 사용해보세요.*/
 public class Resolve5 {
 	public static void main(String[] args) {
-
-
+		int star =5;
+		for(int i=0;i<=star;i++) {
+			//System.out.println("*");
+			for(int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		
 	}
 }
