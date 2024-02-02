6f48f2a2d436c4e95352aabde45c31d44f062ffc
ljw0814 <leejeongwon8145@gmail.com>
2024-02-02 11:06:38 +0900

    임시직 사원과 일용직 사원 메서드 구현

---
+++ Ch09_Extends/src/ch09/abstract10/EmpMenu.java
@@ -0,0 +1,18 @@
+package ch09.abstract10;
+/*	System.out.println("[ 사원 선택 ]");
+	System.out.println("1. 정규직");
+	System.out.println("2. 임시직");
+	System.out.println("3. 일용직");
+	System.out.println("4. 전체정보 보기");
+	System.out.println("5. 종료");
+	System.out.println("번호 입력 >> ");
+ */
+
+public interface EmpMenu {
+	public int REG_EMP = 1;
+	public int TEMP_EMP = 2;
+	public int PART_EMP = 3;
+	public int ALL_INFO = 4;
+	public int EXIT = 5;	
+	
+}
