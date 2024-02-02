480aa0879aa212bbaedec5d68f1168dd75bc0c20
ljw0814 <leejeongwon8145@gmail.com>
2024-02-02 10:16:30 +0900

    viewMenu()와 run() 구현하고 프로그램이 계속 진행되도록 함

--- Test01_Study/src/ch08/class02/Restaurant.java
+++ Test01_Study/src/ch08/class02/Restaurant.java
@@ -7,30 +7,38 @@ public class Restaurant {
 	private String[] foodN = {"스테이크","제로콜라","파스타","샐러드"};
 	private int[] foodPri = {48000,2000,18000,16000};
 	private int selectNum;
-	public Scanner sc;
+	private int totalPay;
+	private Scanner sc = new Scanner(System.in);
+	
 	
 	// 메서드	메뉴판 출력
 	public void viewMain() {
 		System.out.println("--------[메뉴판]--------");
 		for(int i=0; i<foodN.length;i++) {
 			for(int j = 0; j < i; j++) {
-				System.out.println(foodN[i]+foodPri[j]);
+				System.out.println(i+". "+foodN[i]+": "+foodPri[j]);
 			}
 		}
 	}
 	
 	// 주문 받는 메서드
-	public void orderFood(int[] foodPri, int selectNum) {
-		
+	public void orderFood() {
+		System.out.println("주문하실 음식 번호를 입력해주시오: ");
+		selectNum = sc.nextInt();
 	}
 	
 	
 	
 	// 결제금액을 알려주는 메서드
 	public void calc() {
-		total = selectNum * foodpri[];
-		
+		totalPay = selectNum * foodPri[selectNum -1];//사용자가 선택한 selectNum를 배열의 인덱스로 변환하는 것
+		System.out.println(totalPay+"원 결제 도와드리겠습니다.");
 	}
+	
+	public void keyboardEnd() {
+		sc.close();
+	}
+	
 	
 	
 	
--- Test01_Study/src/ch08/class02/RestaurantMain.java
+++ Test01_Study/src/ch08/class02/RestaurantMain.java
@@ -13,26 +13,12 @@ public class RestaurantMain {
 		Scanner sc = new Scanner(System.in);
 		
 		Restaurant rest = new Restaurant();
-		rest.sc = sc;
-		rest.name = "새마을 식당";
-		rest.viewMenu();
-		rest.selectFood();
-		rest.deliveryFood();
 		
-		Restaurant rest1 = new Restaurant();
-		rest1.sc = sc;
-		rest1.name = "낙원타코";
-		rest1.viewMenu();
-		rest1.selectFood();
-		rest1.deliveryFood();
+		rest.viewMain();
+		rest.orderFood();
+		rest.calc();
+		rest.keyboardEnd();
 		
-		Restaurant rest2 = new Restaurant();
-		rest2.sc = sc;
-		rest2.name = "소호정";
-		rest2.viewMenu();
-		rest2.selectFood();
-		rest2.deliveryFood();
-
 		sc.close();
 	}
 }
