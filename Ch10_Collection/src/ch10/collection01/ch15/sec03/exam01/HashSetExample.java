cb2de143a249c6dff6f72db204a2370d086e8838
ljw0814 <leejeongwon8145@gmail.com>
2024-02-06 16:02:09 +0900

    key value, hashCode 추가, Map 구조

--- Ch10_Collection/src/ch10/collection01/ch15/sec03/exam01/HashSetExample.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec03/exam01/HashSetExample.java
@@ -10,6 +10,10 @@ import java.util.*;
  * C = A∪B = {1, 2, 3, 4, 5}
  * 데이터가 입력될 때 기존 데이터가 있는지 확인한다.
  * 이때 사용하는 알고리즘이 Hash알고리즘이다.
+ * 
+ * '검색속도는 빠르다'
+ * '중복 허용 않는다'
+ * '순서 보장되지 않는다'
  * */
 public class HashSetExample {
 	public static void main(String[] args) {
--- Ch10_Collection/src/ch10/collection01/ch15/sec03/exam02/HashSetExample.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec03/exam02/HashSetExample.java
@@ -7,10 +7,21 @@ public class HashSetExample {
 		//HashSet 컬렉션 생성
 		Set<Member> set = new HashSet<Member>();
 
+		Member mem0 = new Member("홍길동", 30);
+		Member mem1 = new Member("홍길동", 30);
+		
+		System.out.println(mem0.hashCode());
+		System.out.println(mem1.hashCode());
+		
 		//Member 객체 저장
-		set.add(new Member("홍길동", 30));
-		set.add(new Member("홍길동", 30));
+		set.add(mem0);
+		set.add(mem1);	// 내부에서 hashCode() -> equals()비교/걸러짐
 
+		//저장된 객체 수 출력
+		System.out.println("총 객체 수 : " + set.size());
+		
+		set.add(new Member("임꺽정", 30));
+		
 		//저장된 객체 수 출력
 		System.out.println("총 객체 수 : " + set.size());
 	}
