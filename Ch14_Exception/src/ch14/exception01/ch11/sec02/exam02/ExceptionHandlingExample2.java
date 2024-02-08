5cb259582eefe75aaf6326ede180a76cca00c5a0
ljw0814 <leejeongwon8145@gmail.com>
2024-02-08 17:58:16 +0900

    0208

--- Ch14_Exception/src/ch14/exception01/ch11/sec02/exam02/ExceptionHandlingExample2.java
+++ Ch14_Exception/src/ch14/exception01/ch11/sec02/exam02/ExceptionHandlingExample2.java
@@ -2,14 +2,21 @@ package ch14.exception01.ch11.sec02.exam02;
 
 public class ExceptionHandlingExample2 {
 	public static void printLength(String data) {
+		// 1)로직 실행 영역
 		try {
 			int result = data.length();
 			System.out.println("문자 수: "+result);
-			}catch(NullPointerException e) {
+		}
+		// 2) try에서 발생한 NullPointerException
+		//	예외를 받아서 처리하는 영역
+		catch(NullPointerException e) {
 				System.out.println(e.getMessage());
 				//System.out.println(e.toString());
 				//e.printStackTrace();		
-			}finally { 
+		}
+		// 3) try가 실행되었는, catch가 실행되었든
+		// 무조건 마무리로 실행되어야 하는 영역
+		finally { 
 				System.out.println("[마무리 실행]\n");
 			}
 		}
--- Test01_Study/src/ch08/class01/Restaurant.java
+++ Test01_Study/src/ch08/class01/Restaurant.java
@@ -1,6 +1,5 @@
 package ch08.class01;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /* 클래스는 영역이 확실하다(클래스의 캡슐화)
@@ -16,54 +15,16 @@ import java.util.Scanner;
  * 필드들의 집합(Vo, Dto)
  * */
 public class Restaurant {
-	// 필드
-	private String[] foods = {"소금빵","푸딩","크레페","초코빵"};
-	private int[] price = {2800,5000,8000,3500};
-	private int selectNum; // 사용자가 선택한 메뉴번호를 저장하는 변수
+	// 필드 + 멤버변수
+	private String[] foods = {"연어덮밥","돈까스","냉모밀","가라아게"};
+	private int count;
 	private Scanner sc = new Scanner(System.in);
-	// foods 전부 출력하는 메서드
-    public void printAllFoods() {
-        System.out.println(Arrays.toString(foods));
-    }
-	
-	// 메서드: 메뉴 출력
+	//메서드
 	public void viewMenu() {
-	    for (int i = 0; i < foods.length; i++) {
-	        System.out.println((i + 1) + "." + foods[i] + ": " + price[i]);
-	    }
+		for(int i =0; i<foods.length; i++) {
+			System.out.println(i+"."+foods);
+		}
 	}
-	 // 사용자의 메뉴 선택을 받는 메서드
-	public void selectFood() {
-		System.out.println("어떤음식을 주문하시겠습니까?");
-		selectNum = sc.nextInt();
-	}
-
-	/*
-	 * public void deliveryFood() { String food = foods[selectNum]; //int price =
-	 * price[selectNum]; System.out.println(food+"로 주문 접수하겠습니다");
-	 * System.out.println(price+"원 결제 도와드리겠습니다."); }
-	 */
-	
-	 // 사용자의 주문을 처리하고 주문 내역을 화면에 표시하는 메서드
-    public void deliveryFood() {
-    	// 배열에서 선택한 음식과 해당하는 가격을 가져옴
-        String food = foods[selectNum - 1];
-        int selectedPrice = price[selectNum - 1];
-        int num = price[0];
-        System.out.println(food + "(으)로 주문 접수하겠습니다");
-        //System.out.println(Arrays.toString(new int[]{selectedPrice}) + "원 결제 도와드리겠습니다.");
-       
-        System.out.println(selectedPrice+ "원 결제 도와드리겠습니다.");
-        // 배열 출력시 Arrays.toString()
-        //System.out.println(price + "원 결제 도와드리겠습니다.");
-        
-    }
-	
-	public void keyboardEnd() {
-		sc.close();
-	}
-	
-	
 	
 }
 
