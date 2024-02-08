9f351a8e494f9c9b0b2f1e5de75f59ec2dbc0970
ljw0814 <leejeongwon8145@gmail.com>
2024-02-08 11:20:06 +0900

    Ch13_Generic 프로젝트의 ch13.sec01의 예제에 주석 처리

--- Ch13_Generic/src/ch13/sec01/GenericExample.java
+++ Ch13_Generic/src/ch13/sec01/GenericExample.java
@@ -1,5 +1,14 @@
 package ch13.sec01;
-
+/* Box<String> box1 = new Box<>();
+ * Box<Integer> box2 = new Box<>();
+ * 위처럼 코드를 작성하면 아래처럼 컴파일러는 자동으로 2개의 클래스를 정의하게 된다.
+ * class Box_String{
+ * 	public String content;
+ * }
+ * class Box_integer{
+ * 	public Integer content;
+ * }
+ * */
 public class GenericExample {
 	public static void main(String[] args) {
 		//Box<String> box1 = new Box<String>();
@@ -10,8 +19,8 @@ public class GenericExample {
 
 		//Box<Integer> box2 = new Box<Integer>();
 		Box<Integer> box2 = new Box<>();
-		box2.content = 100;
-		int value = box2.content;
+		box2.content = 100;			// boxing : heap -> Stack
+		int value = box2.content;	// UnBoxing : Stack -> heap  
 		System.out.println(value);
 	}
 }
